28d97f7f4be6ca14e1ebcf85cfbc17e647ca62b3
J-B-Sulaymonov <hilalteam.uz@gmail.com>
2025-06-30 18:30:15 +0500

    30-06-2025

--- apply/urls.py
+++ apply/urls.py
@@ -8,5 +8,9 @@ urlpatterns = [
     path('ajax/get-study-forms/<int:direction_id>/', views.get_study_forms, name='ajax_get_study_forms'),
     path('api/check-uniqueness/', views.check_uniqueness, name='check_uniqueness'),
     path('check-status/', views.check_application_status, name='check_application_status'),
+    path('du-test/<str:ap_number>/', views.test, name='test'),
+    path('api/save-test-results/', views.save_test_results, name='save_test_results'),
+    # path('submit-test/<str:ap_number>/', views.submit_test, name='submit_test')
+    # path('submit_quiz/', views.submit_quiz_view, name='submit_quiz'),
 
 ]
