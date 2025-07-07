49f93e8ed2367f2cc27c362b3dc0e68b3c4f2418
J-B-Sulaymonov <hilalteam.uz@gmail.com>
2025-07-07 10:14:33 +0500

    07-07-2025

--- apply/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from .models import Question
-
-class QuestionImportForm(forms.ModelForm):
-    upload_file = forms.FileField(required=False, label="Fayldan yuklash")
-
-    class Meta:
-        model = Question
-        fields = '__all__'
